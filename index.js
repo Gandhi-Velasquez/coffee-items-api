65847162bdccbeb7d398d222b8f73aca416d0240
Gandhi <gandhi@involv.app>
2023-10-20 13:59:22 -0700

    Adding api to try out.

---
+++ index.js
@@ -0,0 +1,45 @@
+const PORT = process.env.PORT || 3000
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+async function getCoffees(req, res) {
+  const data = await require('./coffeeData.json')
+  const coffeeItems = data.coffees
+  res.json(coffeeItems)
+}
+
+async function getPastries(req, res) {
+  const data = await require('./pastrieData.json')
+  const pastrieItems = data.pastries
+  res.json(pastrieItems)
+}
+
+async function getMilks(req, res) {
+  const data = await require('./milkData.json')
+  const milkItems = data.milks
+  res.json(milkItems)
+}
+
+async function getTeas(req, res) {
+  const data = await require('./teaData.json')
+  const teaItems = data.teas
+  res.json(teaItems)
+}
+
+async function getFoods(req, res) {
+  const data = await require('./foodData.json')
+  const foodItems = data.foods
+  res.json(foodItems)
+}
+
+app.get('/coffees', getCoffees)
+app.get('/pastries', getPastries)
+app.get('/teas', getTeas)
+app.get('/milks', getMilks)
+app.get('/foods', getFoods)
+
+app.listen(PORT, () => console.log(`Server Running on PORT ${PORT}`));
